a5a16556adf2d07168b4d6e896d03a0f3c3b775f
ma <320775467@qq.com>
2021-09-22 12:47:09 +0800

    new file:   40x.php 	modified:   README.md 	deleted:    index.html

---
+++ 40x.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="zh-CN">
+
+<head>
+    <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+
+<script src="https://v1.hitokoto.cn/?encode=js&select=%23hitokoto" defer></script>
+
+    <link rel="shortcut icon" href="https://z3.ax1x.com/2021/04/22/cqR5wR.png" type="image/x-icon">
+
+</head>
+
+<body>
+<!-- jQuery 和 Bootstrap 集成包（集成了 Popper） -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script async src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></script>
+         
+
+    <div class="container">
+        <br>
+        <div class="alert alert-warning" role="alert">
+            对不起，你正在寻找的页面我们没找到。请尝试检查 URL 是否出错。</div>
+        <div class="card" >
+            <img class="card-img-top " src="http://127.0.0.1:8000/usr/uploads/2021/09/4272388695.jpg"  width="100%" alt="加载中..." >
+    
+            <div class="card-body" >
+                <h3 class="card-title" > 404 Page Not Found </h3>
+
+		<p id="hitokoto"  class="card-text"> </p>
+
+                <a  href="javascript:location.reload();" class="card-link btn btn-primary" >刷新</a>
+            </div>
+            <audio src="http://music.163.com/song/media/outer/url?id=29129632.mp3" loop autoplay >你的浏览器不支持播放音乐</audio>
+            
+        </div>
+	<br>
+       <div class="alert alert-info" role="alert">
+       <span id="busuanzi_container_site_pv">本站总访问量<span id="busuanzi_value_site_pv"></span>次</span>
+       </div>
+       <br>
+    </div>
+</body>
+</html>
+
